4c6c643395103c17911d82059cc76bfcfd0bbbc7
Najma Amla <zaidamla96@gmail.com>
2017-04-20 12:31:11 +0530

    Zaid Commit

--- app/src/main/java/com/zaid/attendance/ListActivity.java
+++ app/src/main/java/com/zaid/attendance/ListActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -22,5 +25,20 @@ public class ListActivity extends AppCompatActivity {
         //setSupportActionBar(toolbar);
         backgroundTask backgroundTask=new backgroundTask(ListActivity.this);
         backgroundTask.execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.submit){
+            Toast.makeText(this, "submit", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
