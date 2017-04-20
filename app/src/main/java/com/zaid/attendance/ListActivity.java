974a0332ab4aeee814eea3fd41d2861b4a1ded49
Najma Amla <zaidamla96@gmail.com>
2017-04-20 11:45:10 +0530

    Zaid Commit

--- app/src/main/java/com/zaid/attendance/ListActivity.java
+++ app/src/main/java/com/zaid/attendance/ListActivity.java
@@ -3,11 +3,12 @@ package com.zaid.attendance;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
     RecyclerView recyclerView;
     ArrayList<student_list> stdlist;
 
@@ -16,27 +17,10 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         recyclerView=(RecyclerView)findViewById(R.id.rv);
+        toolbar=(Toolbar)findViewById(R.id.toolBar);
 
-       // dummydata();
+        //setSupportActionBar(toolbar);
         backgroundTask backgroundTask=new backgroundTask(ListActivity.this);
         backgroundTask.execute();
-
-//        myadapter adapter=new myadapter(this,stdlist);
-//
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        recyclerView.setAdapter(adapter);
     }
-
-//    private void dummydata() {
-//        stdlist=new ArrayList<>();
-//        for(int i=0;i<10;i++){
-//            student_list std = new student_list();
-//            std.setRollno(i+1);
-//            std.setName("Name "+(i+1));
-//            stdlist.add(std);
-//
-//        }
-//        backgroundTask backgroundTask=new backgroundTask();
-//        backgroundTask.execute();
-//    }
 }
